e1dbfb21e741480ad6b1e1111f5aae8e3cc98f8e
LungeloTZungu <lzungu56@gmail.com>
2023-08-15 19:26:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StacksEvent stack = new StacksEvent(10);
+
+            for (int i = 0; i < 3; i++)
+            {
+                stack.Push("Squirtle");
+                stack.Push("Pickachu");
+                stack.Push("Charmander");
+            }
+            stack.Pop();
+            // used to see the value/item at the top
+            Console.WriteLine("Word at top: " + stack.Peek());
+            while (!stack.isEmpty())
+            {
+                var value = stack.Pop();
+                Console.WriteLine(value);
+            }
+             /// ********N.B we can also use ther microsoft version of stacvks which will make the StackEvent class useless since it will do all the work for us
+        }
+    }
+}
---
+++ StacksEvent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class StacksEvent
+    {
+        public int MaxSize { get; set; }  // Array stacks you need a maximum size to init array
+        // this holds our array
+        public string[] stackArray { get; set; }
+        // this keeps track of top
+        public int Top { get; set; }
+
+        public StacksEvent(int size)
+        {
+            // this holds constructure value 
+            this.MaxSize = size;
+            // creates array with size
+            this.stackArray = new string[MaxSize];
+            // We give top -1 because array is 0 index ; if we dont it will skip first element
+            this.Top = -1;
+        }
+
+        public void Push(string item)
+        {
+            // increment top
+            Top++;
+            stackArray[Top] = item;
+
+        }
+
+        public string Pop()
+        {
+            // just a placeholder
+            int oldTop = Top;
+            //decrement  for new top
+            Top--;
+            return stackArray[oldTop];
+        }
+        public string Peek()
+        {
+            return stackArray[Top];
+        }
+
+        public bool isEmpty()
+        {
+            return Top == 0;
+        }
+
+        public bool isFull()
+        {
+            return MaxSize - 1 == Top;
+        }
+
+    }
+}
